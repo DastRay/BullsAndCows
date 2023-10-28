c1f668ba2a43913254268ecc20cec701c8cb0f4c
DastRay <104367556+DastRay@users.noreply.github.com>
2023-10-28 17:10:55 +0300

    Add files via upload

--- main.c
+++ main.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
+#include <time.h>
 #include <locale.h>
 
 #include "functions.h"
 
+#define SIZE 4 // определение длины числа для игры
 
 
+int main() 
+{  
+   setlocale(LC_ALL, "Rus");
 
-int main()
-{
-    setlocale(LC_ALL, "Rus");
-    BullsAndCows();
+   BullsAndCows();
 }
